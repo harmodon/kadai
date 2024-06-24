4e1410504c70e627d0b4901737a5aa3524a8e971
harmodon <harmony.udon0507@gmail.com>
2024-06-24 22:04:26 +0900

    delete diff_2 kadai.py

--- kadai.py
+++ kadai.py
@@ -3,10 +3,8 @@ def main():
     number_2 = int(input("2つ目の自然数を入力してください: "))  
     sum = number_1 + number_2
     diff_1 = number_1 - number_2
-    diff_2 = number_2 - number_1
     print(f'自然数の合計は {sum}')
     print(f'1つめの自然数から2つ目の自然数を引くと {diff_1}')
-    print(f'2つめの自然数から1つ目の自然数を引くと {diff_2}')
 
 if __name__=='__main__':
     main()
