0b623c4232339f6143b6718bea5f9517157c7144
harmodon <harmony.udon0507@gmail.com>
2024-06-24 18:19:50 +0900

    create kadai.py

---
+++ kadai.py
@@ -0,0 +1,8 @@
+def main():
+    number_1 = int(input("1つ目の自然数を入力してください: "))
+    number_2 = int(input("2つ目の自然数を入力してください: "))  
+    sum = number_1 + number_2
+    print(f'自然数の合計は {sum}')
+
+if __name__=='__main__':
+    main()
