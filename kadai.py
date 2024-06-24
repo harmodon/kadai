fa37eb3a8d3812afc2b0a38f21fb6c046b28b1dd
harmodon <harmony.udon0507@gmail.com>
2024-06-24 22:37:20 +0900

    add GP_sum kadai.py

--- kadai.py
+++ kadai.py
@@ -3,18 +3,26 @@ import math as mt
 def main():
     number_1 = int(input("1つ目の自然数を入力してください: "))
     number_2 = int(input("2つ目の自然数を入力してください: "))  
+
     sum = number_1 + number_2
     diff = number_1 - number_2
     prod = number_1 * number_2
     div = number_1 / number_2
+
     gcd = mt.gcd(number_1, number_2)
     lcm = int(prod / mt.gcd(number_1, number_2))
+
+    GP_sum = int(sum * (abs(diff) + 1) / 2)
+
     print(f'2つの自然数の合計は {sum}')
     print(f'1つめの自然数から2つ目の自然数を引くと {diff}')
     print(f'2つの自然数の積は {prod}')
     print(f'1つ目の自然数を2つ目の自然数で割ると {div}')
-    print(f'2つの自然数の最大公約数は{gcd}')
-    print(f'2つの自然数の最小公倍数は{lcm}')
+
+    print(f'2つの自然数の最大公約数は {gcd}')
+    print(f'2つの自然数の最小公倍数は {lcm}')
+
+    print(f'小さい方の自然数から大きい方の自然数までのすべての自然数の和は {GP_sum}')
 
 
 if __name__=='__main__':
